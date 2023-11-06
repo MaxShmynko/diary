b322b0add789c83fcdff66483927560e5d0d2954
Alexander Chernyh <alexchernyh2000@mail.ru>
2023-11-07 02:08:06 +0300

    add slider

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,5 +2,28 @@ import $ from 'jquery';
 import slick from "slick-carousel";
 
 $(() => {
-    $('.promo__slider').slick();
+      $(document).ready(function(){
+        $('.promo__wrap__list').slick({
+          infinite: true, 
+          slidesToShow: 1, 
+          slidesToScroll: 1,
+          vertical: false,
+          variableWidth: true,
+          speed: 500,
+          centerMode: true,
+          prevArrow: $('.promo__wrap__button-prev'),
+          nextArrow: $('.promo__wrap__button-next'),
+        });
+      });
+      $('.content').addClass('disabled');
+
+      $('.promo__list__card-link').click(function(){
+        $('.promo').addClass('disabled');
+        $('.content').removeClass('disabled');
+      });
+
+      $('.content__wrap__header-link').click(function(){
+        $('.content').addClass('disabled');
+        $('.promo').removeClass('disabled');
+      });
 });
