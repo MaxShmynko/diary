1b685dd01d282c75ecddf34b722f3f4fe72427ae
shmunko.maks99 <shmunko.maks99@gmail.com>
2023-11-07 20:29:19 +0300

    counter slick

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,28 +2,35 @@ import $ from 'jquery';
 import slick from "slick-carousel";
 
 $(() => {
-      $(document).ready(function(){
-        $('.promo__wrap__list').slick({
-          infinite: true, 
-          slidesToShow: 1, 
-          slidesToScroll: 1,
-          vertical: false,
-          variableWidth: true,
-          speed: 700,
-          centerMode: true,
-          prevArrow: $('.promo__wrap__button-prev'),
-          nextArrow: $('.promo__wrap__button-next'),
-        });
+    $(document).ready(function(){
+      $('.promo__wrap__list').slick({
+        infinite: true, 
+        slidesToShow: 1, 
+        slidesToScroll: 1,
+        vertical: false,
+        variableWidth: true,
+        speed: 700,
+        centerMode: true,
+        prevArrow: $('.promo__wrap__button-prev'),
+        nextArrow: $('.promo__wrap__button-next'),
       });
+
+      $('.promo__slider').on('afterChange', function(event, slick, currentSlide){
+        let currentSlideIndex = currentSlide + 1;
+        let totalSlides = slick.slideCount;
+        let counterText = currentSlideIndex + '/' + totalSlides;
+        $('.promo__slider__counter-number').text(counterText);
+      });
+    });
+    $('.content').addClass('disabled');
+
+    $('.promo__list__card-link').click(function(){
+      $('.promo').addClass('disabled');
+      $('.content').removeClass('disabled');
+    });
+
+    $('.content__wrap__header-link').click(function(){
       $('.content').addClass('disabled');
-
-      $('.promo__list__card-link').click(function(){
-        $('.promo').addClass('disabled');
-        $('.content').removeClass('disabled');
-      });
-
-      $('.content__wrap__header-link').click(function(){
-        $('.content').addClass('disabled');
-        $('.promo').removeClass('disabled');
-      });
+      $('.promo').removeClass('disabled');
+    });
 });
