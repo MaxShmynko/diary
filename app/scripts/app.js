cb86ffff793f7ed3c3bb375da1725edf8a7d6d5a
shmunko.maks99 <shmunko.maks99@gmail.com>
2023-11-12 15:19:00 +0300

    fix-2

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -38,17 +38,20 @@ $(() => {
   });
     
   function initSlider() {
-    $('.content__wrap__slider-content').slick({
-      infinite: true, 
-      slidesToShow: 1, 
-      slidesToScroll: 1,
-      vertical: false,
-      variableWidth: true,
-      speed: 200,
-      centerMode: true,
-      prevArrow: $('.content__wrap__button-prev'),
-      nextArrow: $('.content__wrap__button-next'),
-    });
+    const sliderContent = $('.content__wrap__slider-content');
+    if (!sliderContent.hasClass('slick-initialized')) {
+      sliderContent.slick({
+        infinite: true, 
+        slidesToShow: 1, 
+        slidesToScroll: 1,
+        vertical: false,
+        variableWidth: true,
+        speed: 200,
+        centerMode: true,
+        prevArrow: $('.content__wrap__button-prev'),
+        nextArrow: $('.content__wrap__button-next'),
+      });
+    }
   }
 
   function destroySlider() {
@@ -71,20 +74,4 @@ $(() => {
   $(window).on('resize', function () {
     handleSliderBasedOnResolution();
   });
-
-  // $(".promo__list__card-link-text").on('click', function(event) {
-  //   event.preventDefault();
-
-  //   let promoCard = $(this).closest(".promo__list__card");
-  //   let cardImageSrc = promoCard.find(".promo__list__card-img img").attr("src");
-  //   let cardTitle = promoCard.find(".promo__list__card-title").text();
-  //   let cardSubtitle = promoCard.find(".promo__list__card-subtitle").text();
-
-
-  //   if (cardTitle && cardSubtitle && cardImageSrc) {
-  //     $(".content__wrap__content-item-image").html('<img src="' + cardImageSrc + '" alt=""/>');
-  //     $(".content__wrap__content-name").text(cardTitle);
-  //     $(".content__wrap__content-surname").text(cardSubtitle);
-  //   }
-  // });
 });
